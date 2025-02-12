90ee28bae3c2974e19eb213d8358be8fd80a434b
Ivan Tran <ivamitran16432@gmail.com>
2025-02-11 18:52:53 -0800

    Guideline comments for database storage protocol

--- hardwareCode/MenuManagementSystemProtocol.py
+++ hardwareCode/MenuManagementSystemProtocol.py
@@ -1,6 +1,23 @@
 class MenuManagementSystemProtocol:
-    from PumpWater import PumpWater
     
+    # Store on database for:
+    # General Example:
+    # "Do Action X": ("[<alias>]-<method name>", <alias>.<method name>)
+    # Row X, Col Y: "[<alias>]-<method name>"
+    # Row X, Col (Y+1): arg1
+    # Row X, Col (Y+2): arg2
+    # ...
+    # Row X, Col (Y+N): argN
+    # Specific Examples:
+    # "Manually Turn on Pump": ("[PumpWater]-manual_turn_on_pump", PumpWater.manual_turn_off_pump)
+    # This:
+    # Row X, Col Y: "[PumpWater]-manual_turn_on_pump"
+    # "Set PWM Duty Cycle": ("[PumpWater]-set_pwm_duty_cycle", PumpWater.set_pwm_duty_cycle)
+    # This:
+    # Row X, Col Y: "[PumpWater]-set_pwm_duty_cycle"
+    # Row X, Col (Y+1): 50 (as an example)
+    
+    from PumpWater import PumpWater
     PumpWater = {
         "Manually Turn on Pump": ("[PumpWater]-manual_turn_on_pump", PumpWater.manual_turn_off_pump),
         "Manually Turn off Pump": ("[PumpWater]-manual_turn_off_pump", PumpWater.manual_turn_off_pump),
