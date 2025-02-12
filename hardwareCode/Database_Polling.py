ac49cf1c18710c75c128678ba328299e63816bc6
Ivan Tran <ivamitran16432@gmail.com>
2025-02-11 20:32:11 -0800

    Updated Database_Polling to better match existing code and some additions

--- hardwareCode/Database_Polling.py
+++ hardwareCode/Database_Polling.py
@@ -2,53 +2,71 @@ from MenuManagementSystem import MenuManagementSystem
 from MYSQL import (retrieve_most_recent_command, retrieve_most_recent_arguments, retrieve_most_recent_func)
 import threading
 import time
+from Status import Status
 
-class DatabasePolling:
-    def __init__(self, menu_system=None):
-        if menu_system is None:
-            print("menu system is not passed in")
-            menu_system = MenuManagementSystem()
-        self.menu = menu_system
-        self.running = False
-        self.thread = None
-    def request(self):
+class DatabaseRequestPollingSystem:
+    
+    class FieldKeys(Status.FieldKeys):
+        REQUEST_POLLING_THREAD_ACTIVE = "requestPollingThreadActive"
+        
+    
+    def __init__(self, menu_management_system : MenuManagementSystem):
+        if menu_management_system == None:
+            raise Exception("Menu Management System is none")
+        self.menu_management_system = menu_management_system
+        
+        # thread variables
+        self.pollingThreadActive = False
+        self.pollingThread = None
+        
+    
+    def retrieveCommand(self):
+        
+        # using MYSQL API functions to get most recent commands and arguments 
         command = retrieve_most_recent_command()
         arguments = retrieve_most_recent_arguments()
+        
+        # countermeasure
+        for i in range(0, len(arguments)):
+            if arguments[i] == 'self':
+                arguments.pop(i)
+                break # expecting at most one instance of 'self'
 
-        self.menu.start_processing()
+        self.menu_management_system.enqueue_command(command, *arguments)
 
-        if len(arguments) == 1:
-            self.menu.enqueue_command(command, arguments[0])
-            print("Arguments: 1")
-        elif len(arguments) == 2:
-            self.menu.enqueue_command(command, arguments[0], arguments[1])
-            print("Arguments: 2")
-        elif len(arguments) == 3:
-            self.menu.enqueue_command(command, arguments[0], arguments[1], arguments[2])
-            print("Arguments: 3")
-        else:
-            self.menu.enqueue_command(command)
-            print("Arguments: 0")
+        # this supports at most 3 arguments
+        # if len(arguments) == 1:
+        #     self.menu_management_system.enqueue_command(command, arguments[0])
+        #     print("Arguments: 1")
+        # elif len(arguments) == 2:
+        #     self.menu_management_system.enqueue_command(command, arguments[0], arguments[1])
+        #     print("Arguments: 2")
+        # elif len(arguments) == 3:
+        #     self.menu_management_system.enqueue_command(command, arguments[0], arguments[1], arguments[2])
+        #     print("Arguments: 3")
+        # else:
+        #     self.menu_management_system.enqueue_command(command)
+        #     print("Arguments: 0")
 
 
 
         #print("Registered commands:", self.menu.options)
 
-    def poll_requests(self):
-        while self.running:
-            self.request()
+    def polling_thread_target(self):
+        while self.pollingThreadActive:
+            self.retrieveCommand()
             time.sleep(5)
 
-    def start_polling(self):
-        if not self.running:
-            self.running = True
-            self.thread = threading.Thread(target=self.poll_requests, daemon=True)
-            self.thread.start()
-            print("DatabasePolling started.")
+    def start_polling_thread(self):
+        if not self.pollingThreadActive:
+            self.pollingThread = threading.Thread(target=self.polling_thread_target, daemon=True) # use daemon for insurance
+            self.pollingThread.start()
+            self.pollingThreadActive = True
 
-    def stop_polling(self):
-        self.running = False
-        self.thread.join()
-        print("DatabasePolling stopped.")
+    def terminate_polling_thread(self):
+        if self.pollingThreadActive:
+            self.pollingThreadActive = False
+            self.pollingThread.join() # wait to join
+            self.pollingThread = None
 
 
