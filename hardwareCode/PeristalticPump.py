e2ae4ab85515c77cdf839d3dd4112b8bf67c5c45
Ivan Tran <ivamitran16432@gmail.com>
2025-03-04 23:31:26 -0800

    Update PeristalticPump.py - Updated things mainly to do with the status object

--- hardwareCode/PeristalticPump.py
+++ hardwareCode/PeristalticPump.py
@@ -11,6 +11,11 @@ import time
 
 class PeristalticPump:
     
+    # Extend the base FieldKeys class for the peristaltic pump
+    class FieldKeys(Status.FieldKeys):
+        PIN = "pin"
+        PUMP_ACTIVE = "pumpActive"
+    
     DEFAULT_DELAY_SECONDS = 1 # the default value for the turnOnWithDuration method's seconds argument
     # it shouldn't be too high to avoid changing the properties of the water too much
     
@@ -23,51 +28,53 @@ class PeristalticPump:
         if isOutermostEntity:
             GPIO_Utility.setModeBCM()
             
-        self.status = Status(isOutermostEntity, debugMode)   
+        self.status = Status(alias, isOutermostEntity, debugMode)
         
         self.pin = pin
-        self.printID = "[" + alias + "]: "
-        self.status.addStatusFieldTuple("alias", alias)
-        self.status.addStatusFieldTuple("pin", pin)
-        self.status.addStatusFieldTuple("pumpActive", False) # True when voltage is being supplied, False when voltage is not being supplied
-        self.status.addStatusFieldTuple("activeDescription", "")
+        # self.status.addStatusFieldTuple(PeristalticPump.FieldKeys.ALIAS, alias) # this one not necessary since alias done by status
+        
+        fieldKeys = [value for key, value in PeristalticPump.FieldKeys.__dict__.items() if not key.startswith("__")]
+        for fieldKey in fieldKeys:
+            self.status.addStatusFieldTuple(fieldKey, None)
+        
+        self.status.setStatusFieldTupleValue(PeristalticPump.FieldKeys.PIN, pin)
+        self.status.setStatusFieldTupleValue(PeristalticPump.FieldKeys.PUMP_ACTIVE, False)
+        
+        # True when voltage is being supplied, False when voltage is not being supplied
         self.onWithDurationSeconds = PeristalticPump.DEFAULT_DELAY_SECONDS # start with the default, can change later on
         
         GPIO_Utility.initializeOutputPin(pin)
 
     def turnOn(self):
-        if(self.status.getStatusFieldTupleValue("pumpActive") == False):
+        if(self.status.getStatusFieldTupleValueUsingKey("pumpActive") == False):
             # turn on the pump
             GPIO.output(self.pin, GPIO.HIGH)
             
             # update status accordingly
-            self.status.setStatusFieldTupleValue("pumpActive", True)
-            self.status.setStatusFieldTupleValue("activeDescription", "On, with no set duration")
+            self.status.setStatusFieldTupleValue(PeristalticPump.FieldKeys.PUMP_ACTIVE, True)
         else:
-            raise Exception(self.printID + "pump wasn't off when trying to turn on")
+            raise Exception("peristaltic pump wasn't off when trying to turn on")
         
     def turnOff(self):
         # note: turn off will only interact with the "on" mode, it won't do anything to "onWithDuration"
-        if(self.status.getStatusFieldTupleValue("pumpActive") == True):
+        if(self.status.getStatusFieldTupleValueUsingKey(PeristalticPump.FieldKeys.PUMP_ACTIVE) == True):
             # turn off the pump
             GPIO.output(self.pin, GPIO.LOW)
             
             # update status accordingly
-            self.status.setStatusFieldTupleValue("pumpActive", False)
-            
-            self.status.setActiveDescription("Off")
+            self.status.setStatusFieldTupleValue(PeristalticPump.FieldKeys.PUMP_ACTIVE, False)
+
         else:
-            raise Exception(self.printID + "pump wasn't on when trying to turn off")
+            raise Exception("peristaltic pump wasn't off when trying to turn on")
         
     def turnOnWithDuration(self, seconds):
         
-        if (self.status.getStatusFieldTupleValue("pumpActive") == False):
+        if (self.status.getStatusFieldTupleValueUsingKey(PeristalticPump.FieldKeys.PUMP_ACTIVE) == False):
             # turn on pump
             GPIO.output(self.pin, GPIO.HIGH)
             
             # update status accordingly
-            self.status.setStatusFieldTupleValue("pumpActive", True)
-            self.status.setStatusFieldTupleValue("activeDescription", "On, with duration of " + str(seconds) + " seconds")
+            self.status.setStatusFieldTupleValue(PeristalticPump.FieldKeys.PUMP_ACTIVE, True)
             
             # initialize timer
             startTime = datetime.datetime.now()
@@ -78,42 +85,37 @@ class PeristalticPump:
             GPIO.output(self.pin, GPIO.LOW)
             
             # update status accordingly
-            self.status.setStatusFieldTupleValue("pumpActive", False)
-            self.status.setStatusFieldTupleValue("activeDescription", "Off")
+            self.status.setStatusFieldTupleValue(PeristalticPump.FieldKeys.PUMP_ACTIVE, False)
         else:
             raise Exception("Tried to turn on pump with duration when already on")
 
-    def threadTarget(self):
-        while self.status.getStatusFieldTupleValue("pumpActive") == False:
-            time.sleep(0.1)
-        self.status.updateStatusDict()
-        self.status.updateStatusString(True)
-        print(self.status.statusString)
-        
-
-def testCase1(pumpPin):
+def test_case_turn_on_with_duration(pumpPin):
     pPumpPin = pumpPin
     
     pump = PeristalticPump(pPumpPin, "pPump", True, False)
     
     input("Input anything to turn on with duration: ")
     
-    pumpThread = th.Thread(target=pump.threadTarget)
-    pumpThread.start()
-    
     pump.turnOnWithDuration(pump.onWithDurationSeconds)
     
-    pumpThread.join()
-
-    pump.status.updateStatusDict()
-    pump.status.updateStatusString(True)
-    print(pump.status.statusString)
-    
     input("Input anything to end program: ")
+    
+    print("This is the final state of the status activity queue")
+    while not pump.status.statusActivityQueue.empty():
+        item = pump.status.statusActivityQueue.get()
+        print(item)
     
     GPIO_Utility.gpioCleanup()
     
     exit()
 
 if __name__ == "__main__":
-    testCase1(20)
+    pumpPH_UP = 22
+    pumpPH_DOWN = 23
+    pumpBASE_A = 21
+    pumpBASE_B = 20
+    
+    # test_case_turn_on_with_duration(20)
+    # test_case_turn_on_with_duration(21)
+    # test_case_turn_on_with_duration(23)
+    test_case_turn_on_with_duration(22)
